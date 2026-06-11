26956ba5bf4b99eca826efe9ceaca00fa2c7f00b
hjiwon203 <hjiwon203@gmail.com>
2026-06-11 10:03:14 +0900

    init

---
+++ src/main/java/com/nhnacademy/flyschedule/SpringAiFlyScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.flyschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiFlyScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiFlyScheduleApplication.class, args);
+    }
+
+}
